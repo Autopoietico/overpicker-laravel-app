29ca5cc5c2ae674afeb013a96352aa7f9a8fb1b3
Autopoiético <nizampop@gmail.com>
2023-10-20 14:49:38 -0500

    Improved hamburguer menu animations

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,22 +1,27 @@
 let headerNav, burgerButton;
 
-function hamburgerValidation(){
+function hamburgerValidation() {
+    headerNav = document.getElementById("header-nav");
+    burgerButton = document.getElementById("burger-menu");
 
-    headerNav = document.getElementById('header-nav');
-    burgerButton = document.getElementById('burger-menu');
-
-    burgerButton.addEventListener('click', hideShow);
+    burgerButton.addEventListener("click", hideShow);
 }
 
-function hideShow(){
-    if(headerNav.classList.contains('invisible')){
-        headerNav.classList.remove('invisible');
-        burgerButton.classList.remove('bg-[#294452]')
-        burgerButton.classList.add('bg-[#1C2E37]')
-    }else{
-        headerNav.classList.add('invisible');
-        burgerButton.classList.remove('bg-[#1C2E37]')
-        burgerButton.classList.add('bg-[#294452]')
+function hideShow() {
+    if (headerNav.classList.contains("invisible")) {
+        headerNav.classList.remove("invisible");
+        headerNav.classList.remove("opacity-0");
+        headerNav.classList.add("opacity-100");
+        burgerButton.classList.remove("bg-[#294452]");
+        burgerButton.classList.add("bg-[#1C2E37]");
+    } else {
+        headerNav.classList.remove("opacity-100");
+        headerNav.classList.add("opacity-0");
+        burgerButton.classList.remove("bg-[#1C2E37]");
+        burgerButton.classList.add("bg-[#294452]");
+        setTimeout(() => {
+            headerNav.classList.add("invisible");
+        }, 300);
     }
 }
 
--- resources/views/components/home/header.blade.php
+++ resources/views/components/home/header.blade.php
@@ -10,7 +10,7 @@
       <p class="poppins text-xs text-white mb-2 border-b-2 border-white">SPEND MORE TIME FOCUSING ON YOUR SURROUNDINGS, STOP TUNNEL VISIONING INDIVIDUAL HEROES.</p>
       <span class="fjalla text-base uppercase">Git Gud</span>
     </div>
-    <nav class="invisible fixed -top-1 bg-[#294452] z-0 w-full py-8 ease-in-out sm:visible sm:static sm:bg-inherit sm:col-span-2 sm:border-t-2 sm:py-3" id="header-nav">
+    <nav class="invisible opacity-0 fixed -top-1 bg-[#294452]/95 z-0 w-full py-8 rounded-b-lg transition-opacity ease-in-out duration-300 sm:visible sm:static sm:bg-inherit sm:col-span-2 sm:border-t-2 sm:py-3" id="header-nav">
       <ol class="grid gap-x-1 gap-y-6 fjalla text-3xl uppercase justify-center text-center sm:grid-flow-col sm:gap-y-0">
         <li><a class="px-2 pt-1 rounded-lg sm:hover:bg-[#294452]" href="/tiers">Tiers</a></li>
         <li><a class="px-2 pt-1 rounded-lg sm:hover:bg-[#294452]" href="/sources">Sources</a></li>
