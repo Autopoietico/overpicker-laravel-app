f873005fdd106308e1c40bd399bede827674bbf2
Autopoiético <nizampop@gmail.com>
2023-10-30 00:39:12 -0500

    Fixed formating and incorrect links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
             Overpicker is a hero composition calculator inspired in
             jazzmasta25's
             <a
-                href="https://www.heropicker.com/"
+                href="https://heropicker.com/"
                 target="_blank"
                 rel="noopener noreferrer"
                 class="underline decoration-amber-400"
@@ -16,15 +16,15 @@
 </section>
 <section class="text-center sm:text-left text-sm max-w-4xl m-auto">
     <div class="mt-12 border-b-2 border-dashed sm:mt-16">
-        <p>
+        <p class="sm:text-lg">
             I'm a casual player of overwatch that love stats and things related
             to best heroes in certain maps, situations, and metas. I'm from
             Colombia and my natural language is <b>Spanish</b>, but I'm know <b>English</b> too.
         </p>
     </div>
     <div class="mt-10 border-b-2 border-dashed">
-        <h2 class="font-normal text-2xl fjalla">Overwatch Experience:</h2>
-        <p class="mt-3">
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">Overwatch Experience:</h2>
+        <p class="mt-3 sm:text-lg">
             I play competitive Overwatch since <b>Season 8</b> of first Overwatch, I touch every
             low rank (even bronze), but now I'm a <b>Diamond I</b> player and I peak to
             3.9k. I mostly play Tanks but I can play all the roles, my mains are
@@ -32,20 +32,20 @@
         </p>
     </div>
     <div class="mt-10 border-b-2 border-dashed">
-        <h2 class="font-normal text-2xl fjalla">Social Links:</h2>
-        <p class="italic">Mostly in Spanish</p>
-        <p class="mt-3"><a href="https://twitter.com/AutopoieticoLP" class="underline decoration-amber-400">Twitter</a></p>
-        <p><a href="https://www.youtube.com/user/SrAutopoietico/" class="underline decoration-amber-400">Youtube</a></p>
-        <p><a href="https://www.instagram.com/autopoietico/" class="underline decoration-amber-400">Instagram</a></p>
-        <p><a href="https://www.reddit.com/user/autopoietico" class="underline decoration-amber-400">Reddit</a></p>
-        <p><a href="https://www.twitch.tv/autopoietico" class="underline decoration-amber-400">Twitch</a></p>
-        <p class="mt-3"><b>Battletag:</b> Autopoietico#1428</p>
-        <p><b>DiscordTag:</b> Autopoietico#1640</p>
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">Social Links:</h2>
+        <p class="italic sm:text-lg">Mostly in Spanish</p>
+        <p class="mt-3 sm:text-lg"><a href="https://twitter.com/AutopoieticoLP" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">Twitter</a></p>
+        <p class="sm:text-lg"><a href="https://www.youtube.com/user/SrAutopoietico/" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">Youtube</a></p>
+        <p class="sm:text-lg"><a href="https://www.instagram.com/autopoietico/" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">Instagram</a></p>
+        <p class="sm:text-lg"><a href="https://www.reddit.com/user/autopoietico" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">Reddit</a></p>
+        <p class="sm:text-lg"><a href="https://www.twitch.tv/autopoietico" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">Twitch</a></p>
+        <p class="mt-3 sm:text-lg"><b>Battletag:</b> Autopoietico#1428</p>
+        <p class="sm:text-lg"><b>DiscordTag:</b> Autopoietico#1640</p>
     </div>
     <div class="mt-10 border-b-2 border-dashed mb-10 sm:mb-14">
-        <h2 class="font-normal text-2xl fjalla">Paypal Email:</h2>
-        <p class="italic">If you want to help me continue with this proyect:</p>
-        <p class="mt-3"><b><a href="https://paypal.me/car930" class="underline decoration-amber-400">nizampop@gmail.com</a></b></p>
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">Paypal Email:</h2>
+        <p class="italic sm:text-lg">If you want to help me continue with this proyect:</p>
+        <p class="mt-3 sm:text-lg"><b><a href="https://paypal.me/car930" class="underline decoration-amber-400" target="_blank" rel="noopener noreferrer">nizampop@gmail.com</a></b></p>
     </div>
 </section>
 @endsection
--- resources/views/sources.blade.php
+++ resources/views/sources.blade.php
@@ -1,1 +1,186 @@
-<h1>Sources</h1>
+@extends('layouts.home') @section('content')
+<section class="mt-12 flex justify-center sm:mt-16">
+    <div class="text-2xl font-black text-center max-w-4xl sm:text-4xl">
+        <p>
+            Overpicker is a hero composition calculator inspired in
+            jazzmasta25's
+            <a
+                href="https://heropicker.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="underline decoration-amber-400"
+                >Hero Picker</a
+            >
+        </p>
+    </div>
+</section>
+<section class="text-center sm:text-left text-sm max-w-4xl m-auto">
+    <div class="mt-10 border-b-2 border-dashed sm:mt-16">
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">
+            Tiers, Map Type, Maps, Counters and Synergies:
+        </h2>
+        <p class="mt-3 sm:text-lg">
+            <b
+                ><a
+                    href="https://heropicker.com/"
+                    class="underline decoration-amber-400"
+                    >Hero Picker</a
+                ></b
+            >
+            was the first source for this picker before, but right now the page
+            has my own data and ideas about interactions in the game.
+        </p>
+    </div>
+    <div class="mt-10 border-b-2 border-dashed">
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">Tiers:</h2>
+        <p class="sm:text-lg">
+            I create a tier list based in the
+            <b
+                ><a
+                    href="https://www.overbuff.com/heroes"
+                    class="underline decoration-amber-400"
+                    >Overbuff</a
+                ></b
+            >
+            stats, this tiers are adjusted taking into consideration the win
+            rate and the pick rate.
+        </p>
+        <p class="sm:text-lg">
+            I use the
+            <b
+                ><a
+                    href="https://t500-aggregator.aryankothari.dev/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >Top 500 Agregator</a
+                ></b
+            >
+            tool to get the top 500 pickrate and best heroes played in the top
+            500 ranking.
+        </p>
+        <p class="sm:text-lg">
+            I check the tierlists created from top500 players like
+            <b
+                ><a
+                    href="https://www.youtube.com/@KarQ"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >KarQ</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@Flats_OW"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >Flats</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@YourOverwatch"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >Freedo</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@Kajor1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >Kajor</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@Toniki"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="underline decoration-amber-400"
+                    >Toniki</a
+                ></b
+            >
+            , and Others
+        </p>
+    </div>
+    <div class="mt-10 border-b-2 border-dashed mb-10 sm:mb-14">
+        <h2 class="font-normal text-2xl fjalla sm:text-3xl">
+            Synergies & Counters::
+        </h2>
+        <p class="sm:text-lg">
+            <b
+                ><a
+                    href="https://www.youtube.com/playlist?list=PLgRMcvKNkYFyD7RyWlNmvYF0NWcKei5z7"
+                    class="underline decoration-amber-400"
+                    >1 Tip for Every Hero Series</a
+                ></b
+            >
+            made for KarQ, is a great inspiration to know counters for every
+            hero in the game.
+        </p>
+        <p class="sm:text-lg">
+            <b
+                ><a
+                    href="https://www.youtube.com/playlist?list=PLfv7DSFO1b69Kb4vzlakeE5MiGL3UKnSh"
+                    class="underline decoration-amber-400"
+                    >Thought Process Series</a
+                ></b
+            >
+            made for SVB have great ideas about what heroes are good with others
+            and also help me knowing other counters.
+        </p>
+        <p class="sm:text-lg">
+            <b
+                ><a
+                    href="https://docs.google.com/document/d/11_VDsXLCrBwogQLaXgoNNbNel3kQkZ6txfT81H7iau0/edit?usp=sharing"
+                    class="underline decoration-amber-400"
+                    >A List on All Existing Compositions in Overwatch</a
+                ></b
+            >
+            made for Shielder_OW, is a good summary of many of the game's most
+            important compositions in the game.
+        </p>
+        <p class="sm:text-lg">
+            <b
+                ><a
+                    href="https://www.youtube.com/watch?v=WfLzVVsHUGI&list=PLxTGkfX26YAguJHHTDFqwuwqLPzBqKXOh"
+                    class="underline decoration-amber-400"
+                    >KarQ Tier Lists</a
+                ></b
+            >, KarQ made a lot of informal Tierlist about sinergies and counters
+            that I take in consideration
+        </p>
+        <p class="sm:text-lg">
+            I'm also inspired for coaches and game theorycrafters like
+            <b
+                ><a
+                    href="https://www.youtube.com/c/StormcrowProductions"
+                    class="underline decoration-amber-400"
+                    >Spillo</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@Kajor1/"
+                    class="underline decoration-amber-400"
+                    >Kajor</a
+                ></b
+            >,
+            <b
+                ><a
+                    href="https://www.youtube.com/@YourOverwatch"
+                    class="underline decoration-amber-400"
+                    >Freedo</a
+                ></b
+            >
+            and others
+        </p>
+    </div>
+</section>
+@endsection
