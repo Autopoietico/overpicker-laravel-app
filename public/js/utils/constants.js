41cb6f0d38394848dabd54b26fe53a80b90fda0c
Autopoiético <nizampop@gmail.com>
2026-05-19 02:06:02 -0500

    Rebalence weighted scores more around sinergies than counters, maps and tiers

--- public/js/utils/constants.js
+++ public/js/utils/constants.js
@@ -23,12 +23,12 @@ const getSelectValue = function (name) {
 //////////////////////
 
 const TIER_MIN = -5;
-const TIER_WEIGHT = 2;
-const COUNTER_WEIGHT = 4 / 5; //Divided by 5 enemy heroes.
+const TIER_WEIGHT = 0.7;
+const COUNTER_WEIGHT = 1 / 5; //Divided by 5 enemy heroes.
 const MIN_COUNTER_VALUE = 20;
-const SINERGY_WEIGHT = 4 / 4; //Divided by 4 allied heroes.
+const SINERGY_WEIGHT = 3 / 4; //Divided by 4 allied heroes.
 const MIN_SINERGY_VALUE = 20;
-const MAPAD_WEIGHT = 5; //MapAD only gives weight to maps now, because ADC is deprecated.
+const MAPAD_WEIGHT = 1.4; //MapAD only gives weight to maps now, because ADC is deprecated.
 const MIN_MAPAD_VALUE = 20;
 
 //////////////////////
