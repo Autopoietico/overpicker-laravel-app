447d20d0584f32d30c7797ffdb73d8893edab23b
Autopoiético <nizampop@gmail.com>
2026-07-02 21:43:22 -0500

    chore: re-balance weights

--- public/js/utils/constants.js
+++ public/js/utils/constants.js
@@ -6,7 +6,7 @@ gain something in the process is a plus.
 Feel free to alter this code to your liking, but please do not re-host it, do not profit from it and do not present it as your own.
 */
 
-const LASTUPDATE = "2025-09-27";
+const LASTUPDATE = "2025-07-02";
 
 //////////////////////
 // Miscelaneus
@@ -23,12 +23,12 @@ const getSelectValue = function (name) {
 //////////////////////
 
 const TIER_MIN = -5;
-const TIER_WEIGHT = 1;
-const COUNTER_WEIGHT = 2 / 5; //Divided by 5 enemy heroes.
+const TIER_WEIGHT = 0.6;
+const COUNTER_WEIGHT = 7 / 5; //Divided by 5 enemy heroes.
 const MIN_COUNTER_VALUE = 20;
-const SINERGY_WEIGHT = 6 / 4; //Divided by 4 allied heroes.
+const SINERGY_WEIGHT = 2 / 4; //Divided by 4 allied heroes.
 const MIN_SINERGY_VALUE = 20;
-const MAPAD_WEIGHT = 3; //MapAD only gives weight to maps now, because ADC is deprecated.
+const MAPAD_WEIGHT = 0.4; //MapAD only gives weight to maps now, because ADC is deprecated.
 const MIN_MAPAD_VALUE = 20;
 
 //////////////////////
